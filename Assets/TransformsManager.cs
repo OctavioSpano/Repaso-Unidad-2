2846c692e84dd20a5459e0a74f0688c7b16cbc38
OctavioSpano <tutispano@gmail.com>
2022-05-19 14:37:40 -0300

    Initial commit

---
+++ Assets/TransformsManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformsManager : MonoBehaviour
+{
+    public GameObject myCube;
+    public GameObject Rotado;
+    public GameObject Escalado;
+    public GameObject RotadoyEscalado;
+
+    public Vector3 originalRotation;
+    public Vector3 originalScale;
+
+    public bool isRotated;
+    public bool isScaled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRotated = false;
+        isScaled = false;
+        originalRotation = myCube.transform.eulerAngles;
+        originalScale = myCube.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isRotated)
+            {
+                myCube.transform.eulerAngles = originalRotation;
+                isRotated = false;
+
+            }
+            else
+            {
+                myCube.transform.eulerAngles = Rotado.transform.eulerAngles;
+                isRotated = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (isScaled)
+            {
+                myCube.transform.localScale = originalScale;
+                isScaled = false;
+            }
+            else
+            {
+                myCube.transform.localScale = Escalado.transform.localScale;
+                isScaled = true;
+            }
+        }
+    }
+}
